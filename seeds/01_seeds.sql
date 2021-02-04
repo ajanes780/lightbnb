3f8bc20ce3605cbb23831dc5727730893ed25d23
Aaron Janes <Aaron.Janes@gmail.com>
2021-02-03 19:52:11 -0700

    seeds

---
+++ seeds/01_seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO reservations (guest_id, property_id, start_date, end_date) 
+VALUES (1, 1, '2018-09-11', '2018-09-26'),
+(2, 2, '2019-01-04', '2019-02-01'),
+(3, 3, '2021-10-01', '2021-10-14');
+
+INSERT INTO users (name,email,) 
+VALUES ('aaron', asoidhjjo@asijop.com,),
+('bob', sdafkjo@dskn.com),
+('tara', akjslnd@asloknm.com);
+
+
+
+INSERT INTO users (name,email,) 
+VALUES ('aaron', asoidhjjo@asijop.com,),
+('bob', sdafkjo@dskn.com),
+('tara', akjslnd@asloknm.com);
