2b13fa60a0d1c006ae71671cbf7c6a81681a47d7
Aaron Janes <Aaron.Janes@gmail.com>
2021-02-04 18:09:42 -0700

    refactor functions for sql

--- seeds/01_seeds.sql
+++ seeds/01_seeds.sql
@@ -3,14 +3,14 @@ VALUES (1, 1, '2018-09-11', '2018-09-26'),
 (2, 2, '2019-01-04', '2019-02-01'),
 (3, 3, '2021-10-01', '2021-10-14');
 
-INSERT INTO users (name,email,) 
-VALUES ('aaron', asoidhjjo@asijop.com,),
-('bob', sdafkjo@dskn.com),
-('tara', akjslnd@asloknm.com);
+INSERT INTO users (name,email,password) 
+VALUES ('aaron', 'asoidhjjo@asijop.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('bob', 'sdafkjo@dskn.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('tara', 'akjslnd@asloknm.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
 
 
 
-INSERT INTO users (name,email,) 
-VALUES ('aaron', asoidhjjo@asijop.com,),
-('bob', sdafkjo@dskn.com),
-('tara', akjslnd@asloknm.com);
+INSERT INTO users (name,email,password) 
+VALUES ('aaron', 'asoidhjjo@asijop.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('bob', 'sdafkjo@dskn.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.'),
+('tara', 'akjslnd@asloknm.com','$2a$10$FB/BOAVhpuLvpOREQVmvmezD4ED/.JBIDRh70tGevYzYzQgFId2u.');
